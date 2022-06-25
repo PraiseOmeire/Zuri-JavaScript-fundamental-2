28fdf1dc528087e4096a9c1b82c787ff2330d774
Praise Omeire <omeireoluomachi99@gmail.com>
2022-06-25 15:13:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter second number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+const options = document.write(num1 + num2);
